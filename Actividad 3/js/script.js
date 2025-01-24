d9cd7f2f0ed7dfee9d5809db388a86d6857f7110
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-01-24 11:49:37 +0100

    Delete Actividad 3 directory

--- Actividad 3/js/script.js
+++
@@ -1,7 +0,0 @@
-function Datos() {
-    let nombre = "" ;
-        nombre = promp("¿Cómo te llamas?")
-    let edad =  ;
-        edad = promp("¿Cuántos años tienes?")
-}
-alert("Hola" + nombre + "veo que tienes" + edad + "años.")
