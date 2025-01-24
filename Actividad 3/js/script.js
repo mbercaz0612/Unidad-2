64891b01025c7ccf503da597996e2548e12879ce
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-01-24 11:52:12 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,8 @@
+function Datos() {
+    let nombre = "" ;
+        nombre = promp("¿Cómo te llamas?")
+    let edad =  ;
+        edad = promp("¿Cuántos años tienes?")
+
+alert("Hola" + nombre + "veo que tienes" + edad + "años.")
+}
