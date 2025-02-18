ac743f91dac71c6aeb6c171dd799d9d9e1836b39
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-02-18 09:56:07 +0100

    Update script.js

--- Actividad 4/js/script.js
+++ Actividad 4/js/script.js
@@ -1,5 +1,7 @@
 function Calcular() {
-  
-
-alert("Hola" + nombre + "veo que tienes" + edad + "años.")
+  let a;
+  let b;
+  a = parseInt(prompt ("Introduce el primer número:"))
+  b = parseInt(prompt ("Introduce el segundo número:"))
+alert("El primer número es: " + a + "\n El segundo número es: " + b + " \n")
 }
