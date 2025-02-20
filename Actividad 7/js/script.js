ce809b74551ef5775444dae56c036a14a4eeb477
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-02-20 22:07:37 +0100

    Update script.js

--- Actividad 7/js/script.js
+++ Actividad 7/js/script.js
@@ -1,21 +1,16 @@
 function Comprobar() {
-    let nota;
-    nota =parseFloat(document.getElementById("a").value, 10);
-    if (nota < 5 ){
+    let nota = parseFloat(document.getElementById("a").value);
+if (nota >= 0 && nota <= 10) {
+    if (nota >= 5) {
+        document.getElementById("resultado").innerHTML = "¡Aprobado!";
+        document.getElementById("resultado").style.color = "green";
+    } else {
         document.getElementById("resultado").innerHTML = "¡Suspenso!";
         document.getElementById("resultado").style.color = "red";
     }
-    if (nota > 4 ){
-        document.getElementById("resultado").innerHTML = "¡Aprobado!";
-        document.getElementById("resultado").style.color = "green";
-
 }
-    if (nota > 10){
-        document.getElementById("resultado").innerHTML = "no es";
-        document.getElementById("resultado").style.color = "blue";
-    }
-    if (nota < 0){
-        document.getElementById("resultado").innerHTML = "no es";
-        document.getElementById("resultado").style.color = "blue";
-    }
+else {
+    document.getElementById("resultado").innerHTML = "no es";
+    document.getElementById("resultado").style.color = "blue";
+}
 }
