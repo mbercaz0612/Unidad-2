6885271b6fabd54cc5cf38f1182b76aa31cc33fd
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-02-20 16:42:20 +0100

    Update script.js

--- Actividad 4/js/script.js
+++ Actividad 4/js/script.js
@@ -1,7 +1,7 @@
 function Calcular() {
   let a;
   let b;
-  a = parseInt(prompt ("Introduce el primer número:"))
-  b = parseInt(prompt ("Introduce el segundo número:"))
+  a = parseInt(prompt ("Introduce el primer número:"));
+  b = parseInt(prompt ("Introduce el segundo número:"));
 alert("El primer número es: " + a + "\n El segundo número es: " + b + " \n La suma es:" +(a + b) +"\n La resta es" +(a - b) + "\n El producto es: " + (a * b) + "\n La división es: " + (a \ b) + "\El resto es: " +(a % b )+ "");
 }
