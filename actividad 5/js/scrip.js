a140f973d56fc74843d2a07e3b4daf78f88e6a18
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-02-21 19:50:39 +0100

    Add files via upload

---
+++ actividad 5/js/scrip.js
@@ -0,0 +1,7 @@
+function Calcular() {
+    
+    let numero1 = parseInt(document.getElementById("numero1").Value, 10);
+    let numero2 = parseInt(document.getElementById("numero2").Value, 10);
+
+    document.getElementById("resultado").innerHTML = "El primer número es: " + numero1 + "\n El segundo número es: " + numero2 + " \n La suma es:" +(anumero1 + numero2) +"\n La resta es" +(a - b) + "\n El producto es: " + (numero1 * numero2) + "\n La división es: " + (numero1 / numero2) + "\nEl resto es: " +(numero1 % numero2)+ "")
+  }
