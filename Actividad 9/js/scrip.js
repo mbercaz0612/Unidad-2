539e7224e84ab8b04b950eb0cd7cc9953957c08e
Martín Berbel Cazorla <mbercaz0612@g.educaand.es>
2025-03-07 11:29:10 +0100

    Add files via upload

--- Actividad 9/js/scrip.js
+++ Actividad 9/js/scrip.js
@@ -7,5 +7,5 @@ function factorial() {
     for ( let cuenta= 1 ;  cuenta <= Numero ; cuenta = cuenta + 1) {
             resultado = resultado * cuenta;
     }
-    alert("El factorial es " + resultado);
+    document.getElementById("resultado").innerHTML = "El factorial es " + resultado;
 }
